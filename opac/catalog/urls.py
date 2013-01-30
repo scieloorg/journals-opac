f8227d24b882e9aabdceecd3f2075048a93e9a2e
fabiobatalha <fabiobatalha@gmail.com>
2013-01-30 18:04:07 -0200

    Template alphabetic

--- opac/catalog/urls.py
+++ opac/catalog/urls.py
@@ -4,5 +4,6 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('',
 
     url(r'^$', direct_to_template, {'template': 'catalog/index.html'}),
+    url(r'^alpha/$', direct_to_template, {'template': 'catalog/alpha.html'}),
 
 )
