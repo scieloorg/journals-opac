90390a162cdddc0cdd842fcbc4ba8c6074a97ec9
Jamil Atta Junior <atta.jamil@gmail.com>
2013-01-31 16:57:48 -0200

    Adicionado view lsit_journal

--- opac/catalog/views.py
+++ opac/catalog/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from catalog import mongomodels
+from django.shortcuts import render_to_response
+
+
+def list_journals(request):
+    journals = mongomodels.list_journals()
+
+    return render_to_response('catalog/alpha.html', {'journals': journals})
+
+
+def list_journals_by_subject(request):
+    pass
