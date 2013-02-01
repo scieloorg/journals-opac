0e32a2d27a20352ef6eecf8b6aee9cabbbdf6321
fabiobatalha <fabiobatalha@gmail.com>
2013-02-01 18:10:14 -0200

    Journal template + mongomodels para journal template

--- opac/catalog/urls.py
+++ opac/catalog/urls.py
@@ -4,12 +4,10 @@ from django.views.generic.simple import direct_to_template
 from catalog import views
 
 urlpatterns = patterns('',
-
     url(r'^$', direct_to_template, {'template': 'catalog/index.html'}),
     url(r'^alpha/$', views.list_journals, name="catalog.list_journals"),
     url(r'^subject/$', views.list_journals_by_subject, name="catalog.list_journals_by_subject"),
     url(r'^usage/$', direct_to_template, {'template': 'catalog/usage.html'}),
-    url(r'^journal/$', direct_to_template, {'template': 'catalog/journal.html'}),
+    url(r'^journal/(?P<journal_id>\d+)/$', views.journal, name='catalog.journal'),
     url(r'^issue/$', direct_to_template, {'template': 'catalog/issue.html'}),
-
 )
--- opac/catalog/views.py
+++ opac/catalog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 
 
 def list_journals(request):
+
     journals = mongomodels.list_journals()
 
     return render_to_response('catalog/alpha.html', {'journals': journals})
@@ -10,3 +11,10 @@ def list_journals(request):
 
 def list_journals_by_subject(request):
     pass
+
+
+def journal(request, journal_id):
+
+    journal = mongomodels.Journal().get_journal(journal_id=journal_id)
+
+    return render_to_response('catalog/journal.html', {'journal': journal})
