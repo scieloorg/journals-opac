9ff4367f3a70dea9568b27e1ad084572c834f0a1
Gustavo Fonseca <gustavofons@gmail.com>
2013-02-18 11:43:52 -0300

    Criação da definição dos modelos CollectionMeta e JournalMeta

--- opac/opac/urls.py
+++ opac/opac/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('catalog.urls')),
 
 )
