0dcc33ee192ced717703ff4419f3d0e2175d1bc2
Jamil Atta Junior <atta.jamil@gmail.com>
2013-02-21 17:25:50 -0300

    Refatorando o template alpha.html garantindo apenas uma query para o mongodb

--- opac/catalog/templatetags/catalogtags.py
+++ opac/catalog/templatetags/catalogtags.py
@@ -6,30 +6,6 @@ from catalog import mongomodels
 register = template.Library()
 
 
-def journal_alpha_list(journals):
-
-    snippet = u'<ul class="unstyled">'
-    last_initial = None
-
-    for journal in journals:
-        if last_initial and journal.title[0].lower() != last_initial.lower():
-            snippet += u'<li>&nbsp;</li>'
-        last_initial = journal.title[0]
-
-        try:
-            abs_url = journal.get_absolute_url()
-        except mongomodels.DocDoesNotExist:
-            abs_url = u'#'
-
-        snippet += u'<li><a href="%s">%s</a> - %s %s</li>' % (abs_url, journal.title, unicode(journal.issues_count), _('issues'))
-
-    snippet += u'</ul>'
-
-    return snippet
-
-register.simple_tag(journal_alpha_list)
-
-
 def journals_by_subject(journals):
 
     snippet = ''
